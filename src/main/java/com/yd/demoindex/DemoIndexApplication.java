28748ca369a094d98a253dc64c39b528f55ce642
朱育东 <3202356389@qq.com>
2023-03-31 13:54:22 +0800

    Initial commit

---
+++ src/main/java/com/yd/demoindex/DemoIndexApplication.java
@@ -0,0 +1,13 @@
+package com.yd.demoindex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoIndexApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoIndexApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/yd/demoindex/DemoIndexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yd.demoindex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoIndexApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
